c703a97e49b7603614b15611dc7d410353173d04
binay-shah <binay18.shah@gmail.com>
2020-05-21 17:42:23 +0545

    build the static version

---
+++ src/BookShelf.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Book from './Book'
+
+const BookShelf = props => {
+
+	return(
+
+		<div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{props.title}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+	                    {props.books.map((book) => <Book book={book} />)}  	                      
+                    </ol>
+                  </div>
+                </div>
+          </div>      
+
+		)
+}
+
+export default BookShelf;
